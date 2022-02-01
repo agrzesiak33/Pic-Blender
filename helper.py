0c55646fed090592cfe19e6e66db9d9649d3ead2
Alex <agrzesiak@hotmail.com>
2022-02-01 16:59:26 -0500

    Creating helper functions -tranfer_main_columns to just move the unblended aread to the new image -add_gradient_left/right to apply the gradient and add the blended pixel to the existing image

---
+++ helper.py
@@ -0,0 +1,79 @@
+import numpy as np
+
+
+def transfer_main_columns(from_pic, blended_pic, start_column, end_column):
+    height = len(from_pic)
+    current_row = 0
+    current_col = start_column
+
+    while current_col < end_column:
+        while current_row < height:
+            blended_pic[current_row][current_col] = np.uint8(from_pic[current_row][current_col])
+            current_row += 1
+
+        current_row = 0
+        current_col += 1
+
+
+def add_gradient_left(pic, blended_pic, left_stop, right_start):
+    # Get percentage of gradient per column
+    gradient_step = 100 / (right_start - left_stop)
+    # Get decimal version of the percent
+    gradient_step /= 100
+
+    height = len(pic[0])
+    total_gradient = 1
+    current_row = 0
+    current_col = right_start
+
+    while current_col > left_stop and total_gradient >= 0:
+        # Apply the gradient correction to all the pixels +
+        current_row = 0
+        while current_row < height:
+            pixel_to_add = pic[current_row][current_col]
+            # Apply the gradient, round it, then convert it back to an 8-bit pixel
+            pixel_to_add = [round(rgb * total_gradient) for rgb in pixel_to_add]
+
+            # In case the pixel already had blending done to it, the gradient needs to be added
+            for value in range(3):
+                projected_rgb_value = blended_pic[current_row][current_col][value] + pixel_to_add[value]
+                if projected_rgb_value > 255:
+                    blended_pic[current_row][current_col][value] = 255
+                else:
+                    blended_pic[current_row][current_col][value] = np.uint8(projected_rgb_value)
+
+            current_row += 1
+        current_col -= 1
+        total_gradient -= gradient_step
+
+
+def add_gradient_right(pic, blended_pic, left_start, right_stop):
+    # Get percentage of gradient per column
+    gradient_step = 100 / (right_stop - left_start)
+    # Get decimal version of the percent
+    gradient_step /= 100
+
+    height = len(pic[0])
+    total_gradient = 1
+    current_row = 0
+    current_col = left_start
+
+    while current_col < right_stop and total_gradient >= 0:
+        # Apply the gradient correction to all the pixels
+        current_row = 0
+        while current_row < height:
+            pixel_to_add = pic[current_row][current_col]
+            # Apply the gradient, round it, then convert it back to an 8-bit pixel
+            pixel_to_add = [round(rgb * total_gradient) for rgb in pixel_to_add]
+
+            # In case the pixel already had blending done to it, the gradient needs to be added
+            for value in range(3):
+                projected_rgb_value = blended_pic[current_row][current_col][value] + pixel_to_add[value]
+                if projected_rgb_value > 255:
+                    blended_pic[current_row][current_col][value] = 255
+                else:
+                    blended_pic[current_row][current_col][value] = np.uint8(projected_rgb_value)
+
+            current_row += 1
+        current_col += 1
+        total_gradient -= gradient_step
