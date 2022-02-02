87c8c5fc260e7e70ea596325fcc62f8da708b227
Alex <agrzesiak@hotmail.com>
2022-02-01 21:46:33 -0500

    Almost done -weird line in output only for left gradient --the pixel is getting added and just getting binned to white - haven't tested the normal things

--- helper.py
+++ helper.py
@@ -1,28 +1,29 @@
 import numpy as np
+import cv2 as cv
 
 
 def transfer_main_columns(from_pic, blended_pic, start_column, end_column):
     height = len(from_pic)
     current_row = 0
-
     for current_col in range(start_column, end_column):
         for current_row in range(height):
             blended_pic[current_row][current_col] = np.uint8(from_pic[current_row][current_col])
 
 
-def add_gradient_left(pic, blended_pic, left_stop, right_start):
+def add_gradient_left(pic, blended_pic, right_start, left_stop):
     # Get percentage of gradient per column
     gradient_step = 100 / (right_start - left_stop)
     # Get decimal version of the percent
     gradient_step /= 100
 
-    height = len(pic[0])
+    height = len(pic)
     total_gradient = 1
     current_row = 0
     current_col = right_start
 
     while current_col > left_stop and total_gradient >= 0:
-        # Apply the gradient correction to all the pixels +
+        #save_image(blended_pic, 'out')
+        # Apply the gradient correction to all the pixels
         for current_row in range(height):
             pixel_to_add = pic[current_row][current_col]
             # Apply the gradient, round it, then convert it back to an 8-bit pixel
@@ -46,7 +47,7 @@ def add_gradient_right(pic, blended_pic, left_start, right_stop):
     # Get decimal version of the percent
     gradient_step /= 100
 
-    height = len(pic[0])
+    height = len(pic)
     total_gradient = 1
     current_row = 0
     current_col = left_start
@@ -68,3 +69,14 @@ def add_gradient_right(pic, blended_pic, left_start, right_stop):
 
         current_col += 1
         total_gradient -= gradient_step
+
+
+def display_image(pic):
+    cv.imshow('dst', pic)
+    cv.waitKey(0)
+    # [display]
+    cv.destroyAllWindows()
+
+
+def save_image(pic, name):
+    cv.imwrite(f'pics/{name}.png', pic)
--- main.py
+++ main.py
@@ -8,15 +8,16 @@ import helper
 HEIGHT = -1
 WIDTH = -1
 NUM_PICS = -1
-SLICE_MAIN_WIDTH = 100 #TODO
+SLICE_MAIN_WIDTH = 10 #TODO
 SLICE_BLEND_WIDTH = 40 #TODO
 ALPHA = -1
 BETA = -1
 
 pics = []
 # Get input from user where the pictures are
-print('Enter folder name')
-input_folder = input().strip()
+#print('Enter folder name')
+input_folder = 'pics'
+#input_folder = input().strip()
 
 files = os.listdir(input_folder)
 
@@ -25,7 +26,7 @@ if files.__sizeof__() == 0:
     exit(-1)
 
 for file in range(len(files)):
-    pics.append(cv.imread(cv.samples.findFile(f'{input_folder}/{files[i]}')))
+    pics.append(cv.imread(cv.samples.findFile(f'{input_folder}/{files[file]}')))
 
 # Ensure all images are the same size
 NUM_PICS = len(pics)
@@ -36,17 +37,18 @@ for pic in pics:
         print('Not all images are the same size')
         exit(-1)
 
-SLICE_BLEND_WIDTH = WIDTH - (NUM_PICS * SLICE_MAIN_WIDTH) #Get all non-main image area
-SLICE_BLEND_WIDTH /= (NUM_PICS - 1) #There are NUM_PICS-1 blend areas
-SLICE_BLEND_WIDTH = round(SLICE_MAIN_WIDTH)
+SLICE_BLEND_WIDTH = WIDTH - (NUM_PICS * SLICE_MAIN_WIDTH) - 1  # Get all non-main image area
+SLICE_BLEND_WIDTH /= (NUM_PICS - 1)  # There are NUM_PICS-1 blend areas
+SLICE_BLEND_WIDTH = round(SLICE_BLEND_WIDTH)
 
 # Final image
-blended_image = np.zeros((WIDTH, HEIGHT, 3), np.uint8)
+blended_image = np.zeros((HEIGHT, WIDTH, 3), np.uint8)
 
-left_main_index_col = 0 # The left most main column of the image we're working on
-right_main_index_col = SLICE_MAIN_WIDTH #The right mose main column of the image
-left_blend_index_col = SLICE_MAIN_WIDTH # The column the gradients will be applied to when going left
-right_blend_index_col = SLICE_MAIN_WIDTH + SLICE_BLEND_WIDTH # The column the gradient will be applied going right
+left_main_index_col = 0  # The left most main column of the image we're working on
+right_main_index_col = SLICE_MAIN_WIDTH - 1  # The right mose main column of the image
+left_blend_index_col = -  1  # The column the gradients will be applied to when going left
+# ^^^^^^^^^^^DOESN'T GET USED UNTIL AFTER FIRST PIC IS PROCESSED
+right_blend_index_col = SLICE_MAIN_WIDTH + SLICE_BLEND_WIDTH  # The column the gradient will be applied going right
 
 for current_pic_index in range(len(pics)):
     # Do first things
@@ -63,20 +65,14 @@ for current_pic_index in range(len(pics)):
     else:
         helper.transfer_main_columns(pics[current_pic_index], blended_image, left_main_index_col, right_main_index_col)
         helper.add_gradient_left(pics[current_pic_index], blended_image, left_main_index_col, left_blend_index_col)
-        helper.add_gradient_left(pics[current_pic_index], blended_image, right_main_index_col, right_blend_index_col)
+        helper.add_gradient_right(pics[current_pic_index], blended_image, right_main_index_col, right_blend_index_col)
 
     left_main_index_col = right_blend_index_col + 1
     right_main_index_col = left_main_index_col + SLICE_MAIN_WIDTH
     left_blend_index_col = left_main_index_col - SLICE_BLEND_WIDTH
     right_blend_index_col = right_main_index_col + SLICE_BLEND_WIDTH
 
-
-
-cv.imshow('pic', pics[1])
-cv.waitKey(0)
-# [display]
-cv.destroyAllWindows()
-
+helper.save_image(blended_image, 'out')
 """print(''' Simple Linear Blender
 -----------------------
 * Enter alpha [0.0-1.0]: ''')
