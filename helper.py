ca580a6b4648d6525db274d4a75ef58f1599729f
Alex <agrzesiak@hotmail.com>
2022-02-18 16:45:36 -0500

    -Making self.main_width a list with all the widths of the main columns...if th econfig file only wants one width this will just be a list of the same value to make it easy - Adding block comments to the beginning of functions - Making the new width locations work with whole image blend - Making it so the 3 tranfering function are inclusive for start and stop.

--- helper.py
+++ helper.py
@@ -10,8 +10,8 @@ class PicMerge:
         self.HEIGHT = -1
         self.BLENDED_WIDTH = -1
         self.BLENDED_HEIGHT = -1
-        self.main_width = -1
-        self.main_locations = []
+        # Main widths are, in all cases, are inclusive on both ends
+        self.main_widths = []
         self.NUM_PICS = -1
         self.blended_pic = []
         self.pics = []
@@ -41,8 +41,18 @@ class PicMerge:
         self.BLENDED_WIDTH = width
 
     def set_main_width(self, main_width):
-        self.main_width = main_width
+        self.main_widths = main_width
 
+    """
+    @ Brief
+        Adds in the main column from source pic to the blended image
+    @ Params
+        [from_pic] : The input picture to add to the blended image
+        [start_column] : The index where we will start adding columns in the blended image.  Inclusive
+        [end_column] : The index where we will end adding columns in the blended image.  Also inclusive
+        [src_start] : (Optional) The left index of the the source picture.  If this is provided it means the columns
+                        we're adding to the blended image have a different start indexes. 
+    """
     def transfer_main_columns(self, from_pic, start_column, end_column, src_start=-1):
         print('Transferring main cols')
         if end_column >= self.BLENDED_WIDTH:
@@ -52,11 +62,22 @@ class PicMerge:
         if current_src_col == -1:
             current_src_col = start_column
 
-        for current_col in range(start_column, end_column):
+        for current_col in range(start_column, end_column+1):
             for current_row in range(height):
                 self.blended_pic[current_row][current_col] = np.uint8(from_pic[current_row][current_src_col])
             current_src_col += 1
 
+    """
+    @ Brief
+        Takes a pic to add to the blended image and applies a gradient proportional to how many columns are between 
+        the start and stop
+    @ Params
+        [pic] : The input picture to add to the blended image
+        [right_start] : The index where we will start adding columns in the blended image.  Inclusive
+        [left_stop] : The index where we will end adding columns in the blended image.  Also inclusive
+        [src_start] : (Optional) The left index of the the source picture.  If this is provided it means the columns
+                        we're adding to the blended image have a different start indexes. 
+    """
     def add_gradient_left(self, pic, right_start, left_stop, src_left_start=-1):
         print('Adding gradient left')
         # Get percentage of gradient per column
@@ -72,7 +93,7 @@ class PicMerge:
         if current_src_col == -1:
             current_src_col = current_col
 
-        while current_col > left_stop and total_gradient >= 0:
+        while current_col >= left_stop and total_gradient >= 0:
             # Apply the gradient correction to all the pixels
             for current_row in range(height):
                 pixel_to_add = pic[current_row][current_src_col]
@@ -91,6 +112,17 @@ class PicMerge:
             current_src_col -= 1
             total_gradient -= gradient_step
 
+    """
+    @ Brief
+        Takes a pic to add to the blended image and applies a gradient proportional to how many columns are between 
+        the start and stop
+    @ Params
+        [pic] : The input picture to add to the blended image
+        [left_start] : The index where we will start adding columns in the blended image.  Inclusive
+        [right_start] : The index where we will end adding columns in the blended image.  Also inclusive
+        [src_right] : (Optional) The left index of the the source picture.  If this is provided it means the columns
+                        we're adding to the blended image have a different start indexes. 
+    """
     def add_gradient_right(self, pic, left_start, right_stop, src_right_start=-1):
         print('Adding Gradient right')
         # Get percentage of gradient per columns
@@ -106,7 +138,7 @@ class PicMerge:
         if current_src_col == -1:
             current_src_col = current_col
 
-        while current_col < right_stop and total_gradient >= 0:
+        while current_col <= right_stop and total_gradient >= 0:
             # Apply the gradient correction to all the pixels
             for current_row in range(height):
                 pixel_to_add = pic[current_row][current_src_col]
@@ -155,77 +187,82 @@ class PicMerge:
             self.pics[1] = right_image
 
     def whole_image_blend(self):
+        # Make sure all images are the same size
         for pic in self.pics:
             if len(pic[0]) != self.WIDTH or len(pic) != self.HEIGHT:
                 print('Not all images are the same size')
                 exit(-1)
 
-        SLICE_BLEND_WIDTH = self.WIDTH - (self.NUM_PICS * self.main_width) - 1  # Get all non-main image area
-        SLICE_BLEND_WIDTH /= (self.NUM_PICS - 1)  # There are NUM_PICS-1 blend areas
-        SLICE_BLEND_WIDTH = round(SLICE_BLEND_WIDTH)
+        variable_main_widths = True
+        if isinstance(self.main_widths, int):
+            variable_main_widths = False
 
-        left_main_index_col = 0  # The left most main column of the image we're working on
-        right_main_index_col = self.main_width - 1  # The right mose main column of the image
-        left_blend_index_col = -  1  # The column the gradients will be applied to when going left
-        # ^^^^^^^^^^^DOESN'T GET USED UNTIL AFTER FIRST PIC IS PROCESSED
-        right_blend_index_col = self.main_width + SLICE_BLEND_WIDTH  # The column the gradient will be applied going right
+        main_locations = []
+        if variable_main_widths:
+            main_locations.append([0, self.main_widths[0] - 1])
+            normalized_main_width = self.WIDTH / (len(self.pics) - 1)
+            current_normal = normalized_main_width
+            # Get all the middle main column locations
+            for pic in range(1, len(self.pics) - 1):
+                # Width of the current main column
+                width = self.main_widths[pic]
+                width += width % 2
+                main_locations.append([current_normal - (width / 2) - 1, current_normal + (width / 2) - 1])
+            # Get the last main column locations
+            main_locations.append([self.WIDTH - self.main_widths[-1], self.WIDTH - 1])
 
+        # Just helper variables to help readability
+        left = 0
+        right = 1
         for current_pic_index in range(self.NUM_PICS):
+            # Transferring the main columns will be ubiquitous with any pic being added
+            self.transfer_main_columns(self.pics[current_pic_index], main_locations[current_pic_index][left],
+                                       main_locations[current_pic_index][right])
             # Do first things
             if current_pic_index == 0:
                 print('Processing the first image')
-                self.transfer_main_columns(self.pics[current_pic_index], left_main_index_col,
-                                           right_main_index_col + 1)
-                self.add_gradient_right(self.pics[current_pic_index], right_main_index_col + 1,
-                                        right_blend_index_col + 1)
+                self.add_gradient_right(self.pics[current_pic_index], main_locations[current_pic_index][right] + 1,
+                                        main_locations[current_pic_index + 1][left] - 1)
 
             # Do last things
             elif current_pic_index == self.NUM_PICS - 1:
                 print('Processing the last image')
-                self.transfer_main_columns(self.pics[current_pic_index], left_main_index_col,
-                                           right_main_index_col + 1)
-                self.add_gradient_left(self.pics[current_pic_index], left_main_index_col - 1,
-                                       left_blend_index_col)
+                self.add_gradient_left(self.pics[current_pic_index], main_locations[current_pic_index][left] - 1,
+                                       main_locations[current_pic_index - 1][right] + 1)
 
             # Do normal things
             else:
                 print(f'Processing the {current_pic_index + 1}th image')
-                self.transfer_main_columns(self.pics[current_pic_index], left_main_index_col,
-                                           right_main_index_col + 1)
-                self.add_gradient_left(self.pics[current_pic_index], left_main_index_col - 1,
-                                       left_blend_index_col)
-                self.add_gradient_right(self.pics[current_pic_index], right_main_index_col + 1,
-                                        right_blend_index_col)
+                self.add_gradient_right(self.pics[current_pic_index], main_locations[current_pic_index][right] + 1,
+                                        main_locations[current_pic_index + 1][left] - 1)
+                self.add_gradient_left(self.pics[current_pic_index], main_locations[current_pic_index][left] - 1,
+                                       main_locations[current_pic_index - 1][right] + 1)
 
-            left_blend_index_col = right_main_index_col + 1
-            left_main_index_col = right_blend_index_col + 1
-            right_main_index_col = left_main_index_col + self.main_width - 1
-            right_blend_index_col = right_main_index_col + SLICE_BLEND_WIDTH
-
+    # TODO make this work list version of main widths
     def middle_only_blend(self):
         #  I want the main width to be even so the math is easier
-        if self.main_width % 2 != 0:
-            self.main_width += 1
+        if self.main_widths % 2 != 0:
+            self.main_widths += 1
 
-        SLICE_BLEND_WIDTH = self.BLENDED_WIDTH - (self.NUM_PICS * self.main_width)  # Get all non-main image area
+        SLICE_BLEND_WIDTH = self.BLENDED_WIDTH - (self.NUM_PICS * self.main_widths)  # Get all non-main image area
         SLICE_BLEND_WIDTH /= (self.NUM_PICS - 1)  # There are NUM_PICS-1 blend areas
         SLICE_BLEND_WIDTH = round(SLICE_BLEND_WIDTH)
 
         # Make sure there are enough pixels on each image to blend
-        room_to_blend = (self.WIDTH - self.main_width) / 2
+        room_to_blend = (self.WIDTH - self.main_widths) / 2
         if room_to_blend < SLICE_BLEND_WIDTH:
             return -1
 
         #  These pointers are only used to keep track of where we are in the blended image  #
         left_main_index_col = 0  # The left most main column of the image we're working on
-        right_main_index_col = self.main_width - 1  # The right mose main column of the image
+        right_main_index_col = self.main_widths - 1  # The right mose main column of the image
         left_blend_index_col = -  1  # The column the gradients will be applied to when going left
         # ^^^^^^^^^^^DOESN'T GET USED UNTIL AFTER FIRST PIC IS PROCESSED
-        right_blend_index_col = self.main_width + SLICE_BLEND_WIDTH  # The column the gradient will be applied going right
+        right_blend_index_col = self.main_widths + SLICE_BLEND_WIDTH  # The column the gradient will be applied going right
 
         #  These pointers are only used to see where we're pulling pixels from in the original images
-        left_source_main = int((self.WIDTH / 2) - (self.main_width / 2))
-        right_source_main = left_source_main + self.main_width - 1  # The -1 is because the front and end index are inclusive
+        left_source_main = int((self.WIDTH / 2) - (self.main_widths / 2))
+        right_source_main = left_source_main + self.main_widths - 1  # The -1 is because the front and end index are inclusive
 
         for current_pic_index in range(self.NUM_PICS):
             # Do first things
@@ -255,30 +292,36 @@ class PicMerge:
 
             left_blend_index_col = right_main_index_col + 1
             left_main_index_col = right_blend_index_col + 1
-            right_main_index_col = left_main_index_col + self.main_width - 1
+            right_main_index_col = left_main_index_col + self.main_widths - 1
             right_blend_index_col = right_main_index_col + SLICE_BLEND_WIDTH
 
-    def load_config(self, filename):
+    def load_config(self, config_filename, pic_folder):
         config = cp.ConfigParser()
-        config.read(filename)
+        config.read(config_filename)
+
+        self.load_pics(pic_folder)
 
         self.out_name = config['general']['out name']
 
         match config.get(['general']['type']).strip().lower():
             case 'stock':
+                # If you want variable main widths
                 if config.getboolean(['general']['variable main widths']):
                     # Use self.main_width to hold a list with many widths
-                    self.main_width = []
+                    self.main_widths = []
                     main_widths = config.options('main widths')
                     for main_width in main_widths:
-                        self.main_width.append(config.getint('main widths', main_width))
+                        self.main_widths.append(config.getint('main widths', main_width))
+                # If all the widths are the same
                 else:
-                    self.main_width = config.getint('general', 'main width')
+                    main_width = config.getint('general', 'main width')
+                    for pic in range(len(self.pics)):
+                        self.main_widths.append([main_width])
             case 'sslectivestock':
                 print('')
             case 'panorama':
                 print('')
-        if isinstance(self.main_width, list):
+        if isinstance(self.main_widths, list):
             print('NOT SURE WHAT TO DO HERE YET')
         options = config.options('main locations')
         for option in options:
